54853902cfc048e6829752547cbd6f1f523ce326
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-05-09 17:59:06 +0530

    added login functionality

--- web_diary/diary_entry/views.py
+++ web_diary/diary_entry/views.py
@@ -71,10 +71,10 @@ def loginPage(request):
     
     else:
         if request.method == "POST":
-            email = request.POST.get('email')
+            username = request.POST.get('username')
             password = request.POST.get('password')
 
-            user = authenticate(request, email=email, password=password)
+            user = authenticate(request, username=username, password=password)
 
             if user is not None:
                 login(request, user)
