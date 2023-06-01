08fe3a963d3c64c61f1cc268e44f3c1442c28090
Arunima Shukla <arunimashukla2003@gmail.com>
2023-06-01 22:20:01 +0530

    models

--- web_diary/diary_entry/models.py
+++ web_diary/diary_entry/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Entry(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     # "User" is on the one side of the relationship and "Entry" is on the many side using ForeignKey. Therefore, for every User, there are many Entrys.
 
@@ -15,7 +15,7 @@ class Entry(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     content = models.TextField()
 
-    # "models.SET_NULL" means that when you delete a particular mood, its value will get equal to null. And "blank=True" ensures that we can submit an empty field in the form.
+    # "null=True" allows the field to have a "NULL" value in the database. And "blank=True" ensures that we can submit an empty field in the form.
 
     def __str__(self):
         return f"{self.date_created.date()}"
